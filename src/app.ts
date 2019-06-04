bcd29b472b9e32437da9eb8df47a8733dc9878af
Lautaro Cozzani <lautaromail@gmail.com>
2019-06-04 16:57:17 -0300

    store pings

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { ProjectDAO, init, close } from './model';
+import { ProjectDAO, init, close, PingDAO } from './model';
 import { PingService } from './ping.service';
 const app = express();
 
@@ -15,16 +15,19 @@ app.get('/', async (req, res) => {
     const projects = await new ProjectDAO().findAll();
     const responses = await Promise.all(projects.map(proj => new PingService().ping(proj.url)));
     responses.forEach((res, idx) => console.log(`response`, projects[idx].name, res.success, res.status, res.time, res.response));
-
+    Promise.all(responses.map((res, idx) => new PingDAO().insert(res, projects[idx]._id)));
+    const stats = await new PingDAO().stats();
+    
     res.send([
         '<html><body>',
         '<center><h1>Project Status</h1></center>',
         '<table style="width: 50%; margin-left: auto; margin-right: auto; margin-top: 3em;" border="1">',
-            '<thead style="text-align: left;"><tr><th>Project</th><th>Status</th><th>Last Updated</th><th>Delay</th></tr></thead>',
+            '<thead style="text-align: left;"><tr><th>Project</th><th>Status</th><th>Last Updated</th><th>Last Delay</th><th>AVG Delay</th><th>Count</th><th>Fails</th></tr></thead>',
             '<tbody>',
-            responses.map((res, idx) =>
-                `<tr><tr><td>${projects[idx].name}</td><td>${res.success ? 'OK' : 'FAIL'} (${res.status})</td><td>${new Date().toLocaleString()}</td><td>${res.time}ms</td></tr></tr>`
-            ).join(''),
+            responses.map((res, idx) =>{
+                const stat = stats.find(s => s._id.toString() === projects[idx]._id.toString()) || {} as any;
+                return `<tr><tr><td>${projects[idx].name}</td><td>${res.success ? 'OK' : 'FAIL'} (${res.status})</td><td>${new Date().toLocaleString()}</td><td>${res.time}ms</td><td>${Math.round(stat.avg)}ms</td><td>${stat.count}</td><td>${stat.fails} (${Math.round(100 * stat.fails/stat.count)}%)</td></tr>`
+            }).join(''),
             '</tbody>',
         '</table>',
         '</body></html>'
--- src/model.ts
+++ src/model.ts
@@ -1,4 +1,5 @@
 import { MongoClient, Db } from 'mongodb';
+import { PingResponse } from './ping.service';
 
 const client = new MongoClient(process.env.MONGO_URL);
 
@@ -13,6 +14,7 @@ export function close() {
 }
 
 export interface Project {
+    _id?: any;
     name: string;
     url: string;
 }
@@ -25,7 +27,43 @@ export class ProjectDAO {
         this.db = client.db('still-alive');
     }
 
-    async findAll() {
+    findAll() {
         return this.db.collection<Project>('projects').find().toArray();
+    }
+}
+
+export class PingDAO {
+
+    private db: Db;
+
+    constructor() {
+        this.db = client.db('still-alive');
+    }
+
+    insert(ping: PingResponse, _id: any) {
+        return this.collection.insertOne({
+            timestamp: new Date(),
+            success: ping.success,
+            status: ping.status,
+            time: ping.time,
+            project: _id
+        });
+    }
+
+    stats() {
+        return this.collection.aggregate<any>([
+            {
+                $group: {
+                    _id: '$project',
+                    count: { $sum: 1 },
+                    avg: { $avg: '$time' },
+                    fails: { $sum: { $cond: { if: '$success', then: 0, else: 1 } } }
+                }
+            }
+        ]).toArray();
+    }
+
+    private get collection() {
+        return this.db.collection('pings');
     }
 }
