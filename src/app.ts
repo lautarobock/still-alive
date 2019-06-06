c63b3fa2ad8b82f73883a4b856bdf5626be280e2
Lautaro Cozzani <lautaromail@gmail.com>
2019-06-06 09:47:06 -0300

    ping one by one and add styles

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import * as express from 'express';
 import { ProjectDAO, init, close, PingDAO } from './model';
-import { PingService } from './ping.service';
 const app = express();
 
 init()
     .then(async () => {
         console.log('MONGODB CONECTED');
+        app.use(express.static('public'))
         app.listen(process.env.PORT || 3000, () => console.log('Serveer ready'));
     })
     .catch(err => console.error('Error MONGODB', err));
@@ -20,14 +20,26 @@ app.get('/', async (req: express.Request, res: express.Response) => {
     const stats = await new PingDAO().stats();
     
     res.send([
-        '<html><body>',
+        `<html>
+        <head>
+            <link rel="stylesheet" href="css/styles.css">
+        </head>
+        <body>`,
         '<center><h1>Project Status</h1></center>',
-        '<table style="width: 50%; margin-left: auto; margin-right: auto; margin-top: 3em;" border="1">',
+        '<table>',
             '<thead style="text-align: left;"><tr><th>Project</th><th>Status</th><th>Last Updated</th><th>Last Alive</th><th>Last Delay</th><th>AVG Delay</th><th>Count</th><th>Fails</th></tr></thead>',
             '<tbody>',
             projects.map((project, idx) =>{
                 const stat = stats.find(s => s._id.toString() === project._id.toString()) || {} as any;
-                return `<tr><tr><td>${projects[idx].name}</td><td>${stat.success ? 'OK' : 'FAIL'} (${stat.status})</td><td>${new Date(stat.timestamp).toLocaleString()}</td><td>${new Date(project.lastAlive).toLocaleString()}</td><td>${stat.time}ms</td><td>${Math.round(stat.avg)}ms</td><td>${stat.count}</td><td>${stat.fails} (${Math.round(100 * stat.fails/stat.count)}%)</td></tr>`
+                return `<tr>
+                    <tr><td>${projects[idx].name}</td>
+                    <td class="${stat.success ? 'success' : 'fail'}">${stat.success ? 'OK' : 'FAIL'} (${stat.status})</td>
+                    <td>${new Date(stat.timestamp).toLocaleString()}</td>
+                    <td>${new Date(project.lastAlive).toLocaleString()}</td>
+                    <td>${stat.time}ms</td>
+                    <td>${Math.round(stat.avg)}ms</td>
+                    <td>${stat.count}</td><td>${stat.fails} (${Math.round(100 * stat.fails/stat.count)}%)</td>
+                </tr>`
             }).join(''),
             '</tbody>',
         '</table>',
--- src/jobs.ts
+++ src/jobs.ts
@@ -5,23 +5,7 @@ import { MailService } from "./mail.service";
 init()
     .then(async () => {
         try {
-            const projects = await new ProjectDAO().findAll();
-            const responses = await Promise.all(projects.map(proj => new PingService().ping(proj.url)));
-            responses.forEach((res, idx) => console.log(`response`, projects[idx].name, res.success, res.status, res.time, res.response));
-            const insertResponse = await Promise.all(responses.map((res, idx) => 
-                Promise.all([
-                    new PingDAO().insert(res, projects[idx]._id),
-                    new ProjectDAO().updateIsAlive(projects[idx]._id, res.success)
-                ])
-            ));
-            console.log('insertResponse', insertResponse);
-            const toNotify = await new ProjectDAO().findNotAliveToNotify();
-            
-            await Promise.all(toNotify.map(res => new MailService().send(
-                res.notifications,
-                `${res.name} acaba de fallar`,
-                `El proyecto ${res.name} acaba de registrar un error, es posible que este caido, por favor pongase en contacto con su administrador`
-            )));
+            await new JobRunner().run();
         } catch (err) {
             console.error(err);
         } finally {
@@ -29,3 +13,32 @@ init()
         }
     })
     .catch(err => console.error('Error MONGODB', err));
+export class JobRunner {
+
+    async run() {
+        await this.pingAllProjects();
+        return await this.notifyAllNotAlive();
+    }
+
+    private async pingAllProjects() {
+        const projects = await new ProjectDAO().findAll();
+        for (let idx = 0; idx < projects.length; idx++) {
+            const res = await new PingService().ping(projects[idx].url);
+            console.log(`response`, projects[idx].name, res.success, res.status, res.time, res.response);
+            const insertRes = await new PingDAO().insert(res, projects[idx]._id);
+            console.log('insert', projects[idx].name, insertRes);
+            const updateRes = await new ProjectDAO().updateIsAlive(projects[idx]._id, res.success);
+            console.log('update', projects[idx].name, updateRes);
+        }
+    }
+
+    private async notifyAllNotAlive() {
+        const toNotify = await new ProjectDAO().findNotAliveToNotify();
+
+        await Promise.all(toNotify.map(res => new MailService().send(
+            res.notifications,
+            `${res.name} acaba de fallar`,
+            `El proyecto ${res.name} acaba de registrar un error, es posible que este caido, por favor pongase en contacto con su administrador`
+        )));
+    }
+}
